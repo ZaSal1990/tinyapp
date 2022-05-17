d01e608fdcfca4f5635583661791730690c28361
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-05-17 11:19:57 -0600

    add user authentication on all routes

--- express_server.js
+++ express_server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const cookieSession = require('cookie-session');
-const morgan = require('morgan');
 const bcrypt = require('bcryptjs');
 const bodyParser = require("body-parser");
 const app = express(); //creates interface (constructor)
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 //helper functions require--------------------------------------------------------------------//
 
@@ -22,7 +21,6 @@ const users = {};
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({ keys : ['user_id']}));
-app.use(morgan('dev'));
 app.set("view engine", "ejs"); //to enable EJS, set its as view engine
 
 
@@ -30,8 +28,7 @@ app.set("view engine", "ejs"); //to enable EJS, set its as view engine
 
 app.get("/", (req, res) => { //default route
   if (req.session.user_id) {
-    req.session = null; //destroying session from before server was restarted
-    res.redirect('/login');
+    res.redirect('/urls');
   }
   res.redirect('/login'); //ensuring no access unless you're a registered user
 });
@@ -41,14 +38,14 @@ app.get("/urls", (req, res) => {
     res.redirect('/login');
   } else {
     const templateVars = { //passing relevant items to urls_index ejs template
-      urls : returnURLSforAUser(req.session.user_id, urlDatabase),
+      urls : returnURLSforAUser(req.session.user_id, urlDatabase), //to display URL list for the user
       user : users[req.session.user_id],
     };
     res.render("urls_index", templateVars);
   }
 });
 
-app.get("/urls/new", (req, res) => {
+app.get("/urls/new", (req, res) => { //landing page for when user wants to create new user
   if (!req.session.user_id) {
     res.redirect('/login');
   } else {
@@ -60,63 +57,92 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {//landing page when shortURL is accessed
   if (!req.session.user_id) {
     res.redirect('/login');
+  } else if (req.session.user_id !== urlDatabase[req.params.shortURL].userID) {
+    res.status(403).send(`Error: User not authorized`);
   } else {
-    const templateVars = {
-      shortURL: req.params.shortURL,
-      urls : returnURLSforAUser(req.session.user_id, urlDatabase),
-      user : users[req.session.user_id],
-    };
-    res.render("urls_show", templateVars);
+    if (req.params.shortURL in urlDatabase && urlDatabase[req.params.shortURL].longURL) { //if $this short URL is present in url database against $this userID for any long URL
+      const templateVars = {
+        shortURL: req.params.shortURL,
+        urls : returnURLSforAUser(req.session.user_id, urlDatabase),
+        user : users[req.session.user_id],
+      };
+      res.render("urls_show", templateVars);
+    }
   }
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get("/u/:shortURL", (req, res) => {//reditrection from short to long URL
   if (!req.session.user_id) {
     res.redirect('/login');
-  } else {
+  } else if (req.session.user_id === urlDatabase[req.params.shortURL].userID && urlDatabase[req.params.shortURL].longURL) {//if Long URL exists for $this short URL for $this particular user ID
     const longURL = urlDatabase[req.params.shortURL].longURL; //extracting longURL from urlDatabase
     res.redirect(longURL);
   }
 });
 
 app.get("/register", (req, res) => {
-  const templateVars = { //adding to user database and passing data to ejs render
-    user : users[req.session.user_id],
-  };
-  res.render("register", templateVars);
+  if (users[req.session.user_id]) { //user exists already in users databse
+    res.redirect('/urls');
+  } else {
+    const templateVars = { //adding to user database and passing data to ejs render
+      user : users[req.session.user_id],
+    };
+    res.render("register", templateVars);
+  }
 });
 
 app.get("/login", (req, res) => {
-  const templateVars = {
-    user: req.session.user_id,
-  };
-  res.render("login", templateVars);
+  if (users[req.session.user_id]) { //user exists already in users databse
+    res.redirect('/urls');
+  } else {
+    const templateVars = {
+      user: req.session.user_id,
+    };
+    res.render("login", templateVars);
+  }
 });
 
-//POSET Routes --------------------------------------------------------------------//
+//POST Routes --------------------------------------------------------------------//
 
 app.post("/urls", (req, res) => {//POST action: creates urlDatabase with form submission data at /urls
-  let newshortURL = generateRandomString();
-  urlDatabase[newshortURL] = {
-    longURL : req.body.longURL,
-    userID : req.session.user_id
-  };
-  res.redirect(`/urls/${newshortURL}`); //redirects to shortURL page
+  if (!req.session.user_id) {
+    res.redirect('/login');
+  } else {
+    let newshortURL = generateRandomString();
+    urlDatabase[newshortURL] = {
+      longURL : req.body.longURL,
+      userID : req.session.user_id
+    };
+    res.redirect(`/urls/${newshortURL}`); //redirects to shortURL page
+  }
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {//route to delete request
-  let itemToBeDeleted = req.params.shortURL;
-  delete urlDatabase[itemToBeDeleted];
-  res.redirect(`/urls`);//redirecting to route 
+  if (!req.session.user_id) {
+    res.redirect('/login');
+  } else if (req.session.user_id !== urlDatabase[req.params.shortURL].userID) {
+    res.status(403).send(`Error: User not authorized`);
+  } else {
+    let itemToBeDeleted = req.params.shortURL;
+    delete urlDatabase[itemToBeDeleted];
+    res.redirect(`/urls`);//redirecting to route
+  }
 });
 
 app.post("/urls/:id", (req, res) => {//route to update URL
-  let itemToBeUpdated = req.params.id;
-  urlDatabase[itemToBeUpdated].longURL = req.body.longURL;
-  res.redirect(`/urls/${itemToBeUpdated}`);
+  if (!req.session.user_id) {
+    res.redirect('/login');
+  } else if (req.session.user_id !== urlDatabase[req.params.id].userID) {
+    res.status(403).send(`Error: User not authorized`);
+  } else {
+    let itemToBeUpdated = req.params.id;
+    if (req.body.longURL) {
+      urlDatabase[itemToBeUpdated].longURL = req.body.longURL;
+    } res.redirect(`/urls/${itemToBeUpdated}`);
+  }
 });
 
 app.post("/register", (req, res) => {//route to login post submission for registration
@@ -146,7 +172,6 @@ app.post("/login", (req, res) => {
   if (!req.body.email || !req.body.password) {
     res.status(400).send(`Error: ${res.statusCode} - Invalid data input`);
   } else {
-    console.log('from inside /login post route', users);
     let databaseSearch = ifCredentialsMatchedReturnUser(newemail, newpassword, users); //chceking is its a registered user
     if (databaseSearch.error) {
       res.status(403).send(`Error: ${res.statusCode} ${databaseSearch.error}`);
@@ -167,4 +192,3 @@ app.post("/logout", (req, res) => {//route to destroy session upon logout
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
